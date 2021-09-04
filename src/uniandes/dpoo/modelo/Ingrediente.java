3141a61bdecc55614978c0fdee61e085006192aa
martin-2k <85652994+martin-2k@users.noreply.github.com>
2021-09-03 23:41:01 -0500

    hi

--- src/uniandes/dpoo/modelo/Ingrediente.java
+++ src/uniandes/dpoo/modelo/Ingrediente.java
@@ -1,32 +1,20 @@
 package uniandes.dpoo.modelo;
 
-public class Ingrediente {
 public class Ingrediente{
 	
-	String nombre;
-	//HOLAAAAAAA
-	int costoAdicional;
 	private String nombre;
 	private int costoAdicional;
 	
-	
-	public Ingrediente(String nombre, int costoAdicional) {
-		this.nombre = nombre;
-		this.costoAdicional = costoAdicional;
 	public Ingrediente(String nombre_input, int costoAdicional_input) {
 		// TODO Auto-generated constructor stub
 		nombre = nombre_input;
 		costoAdicional = costoAdicional_input;
 	}
-	
-	public String getNombre() {
 
 	public String get_nombre() {
 		// TODO Auto-generated method stub
 		return nombre;
 	}
-	
-	public int getCostoAdicional() {
 
 	public int get_precio() {
 		// TODO Auto-generated method stub
